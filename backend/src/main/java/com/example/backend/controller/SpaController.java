d34b67907c05d1b81b6930bb9f6ad37d34c747dc
Time_ON <haas.timeon@gmail.com>
2024-07-03 09:09:37 +0200

    SPAController update

--- backend/src/main/java/com/example/backend/controller/SpaController.java
+++ backend/src/main/java/com/example/backend/controller/SpaController.java
@@ -1,63 +1,13 @@
 package com.example.backend.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
-@RequiredArgsConstructor
+@Controller
 public class SpaController {
 
-    @GetMapping("/")
-    public RedirectView redirectFromHomepage() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/login")
-    public RedirectView redirectFromLogin() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/register")
-    public RedirectView redirectFromRegister() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/user-home/settings")
-    public RedirectView redirectFromSettings() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/user-home/shop")
-    public RedirectView redirectFromShop() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/user-home/tic-tac-toe")
-    public RedirectView redirectFromTictactoe() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/user-home")
-    public RedirectView redirectFromUserhome() {
-        return new RedirectView("/index.html");
-    }
-
-    //Error code's
-
-    @GetMapping("/401")
-    public RedirectView redirectFrom401() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/403")
-    public RedirectView redirectFrom403() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/404")
-    public RedirectView redirectFrom404() {
-        return new RedirectView("/index.html");
+    @RequestMapping({"/", "/login", "/register", "/user-home/**", "/401", "/403", "/404"})
+    public String redirect() {
+        return "forward:/index.html";
     }
 }
