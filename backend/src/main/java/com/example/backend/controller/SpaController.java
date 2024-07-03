1ddc55d362d6c21acb4f6c2c5d2ea3fa664c1f15
Time_ON <125261784+Time-0N@users.noreply.github.com>
2024-07-03 08:49:07 +0200

    Update SpaController.java

--- backend/src/main/java/com/example/backend/controller/SpaController.java
+++ backend/src/main/java/com/example/backend/controller/SpaController.java
@@ -2,62 +2,18 @@ package com.example.backend.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/")
 public class SpaController {
 
-    @GetMapping("/")
-    public RedirectView redirectFromHomepage() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/login")
-    public RedirectView redirectFromLogin() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/register")
-    public RedirectView redirectFromRegister() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/user-home/settings")
-    public RedirectView redirectFromSettings() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/user-home/shop")
-    public RedirectView redirectFromShop() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/user-home/tic-tac-toe")
-    public RedirectView redirectFromTictactoe() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/user-home")
-    public RedirectView redirectFromUserhome() {
-        return new RedirectView("/index.html");
-    }
-
-    //Error code's
-
-    @GetMapping("/401")
-    public RedirectView redirectFrom401() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/403")
-    public RedirectView redirectFrom403() {
-        return new RedirectView("/index.html");
-    }
-
-    @GetMapping("/404")
-    public RedirectView redirectFrom404() {
+    @GetMapping({"", "/login", "/register", "/user-home", "/user-home/settings",
+            "/user-home/shop", "/user-home/tic-tac-toe", "/401", "/403", "/404"})
+    public RedirectView redirectAll() {
         return new RedirectView("/index.html");
     }
 }
